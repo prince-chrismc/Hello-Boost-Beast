fba6fd6e8c0d1c8b3b10533f00492c2d473cd0eb
Christopher McArthur <prince.chrismc@gmail.com>
2019-09-16 07:21:16 -0400

    moving install vcpkg to a script

---
+++ ci/install-vcpkg.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ ! -d "$1" ]; then
+   echo "usage: $0 <PATH>"
+fi
+
+if [ ! -d "vcpkg" ]; then
+   git clone https://github.com/Microsoft/vcpkg.git &&
+   cd vcpkg &&
+   ./bootstrap-vcpkg.sh &&
+   sudo ./vcpkg integrate install &&
+   git apply -v --ignore-whitespace "$1"/vcpkg.patch
+else
+   cd vcpkg &&
+   git pull;
+fi
