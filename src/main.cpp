0044cf20653aaba69e04d8ad0ac5053c6cbd5312
Chris Mc <prince.chrismc@gmail.com>
2019-09-15 23:29:42 -0400

    [wip] minor changes for basic HTTP/1.1 support

--- src/main.cpp
+++ src/main.cpp
@@ -27,58 +27,45 @@ SOFTWARE.
 #include <boost/beast.hpp>
 #include <iostream>
 
-namespace ip = boost::asio::ip;         // from <boost/asio.hpp>
-using tcp = boost::asio::ip::tcp;       // from <boost/asio.hpp>
-namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
+namespace ip = boost::asio::ip;		 // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;	// from <boost/asio.hpp>
+namespace http = boost::beast::http; // from <boost/beast/http.hpp>
 
 namespace my_program_state
 {
-	std::size_t
-		request_count()
-	{
-		static std::size_t count = 0;
-		return ++count;
-	}
-
-	std::time_t
-		now()
-	{
-		return std::time(0);
-	}
+std::size_t request_count()
+{
+	static std::size_t count = 0;
+	return ++count;
 }
+
+std::time_t now()
+{
+	return std::time(0);
+}
+} // namespace my_program_state
 
 class http_connection : public std::enable_shared_from_this<http_connection>
 {
 public:
-	http_connection(tcp::socket socket)
-		: socket_(std::move(socket))
+	http_connection(tcp::socket socket) : socket_(std::move(socket))
 	{
 	}
 
 	// Initiate the asynchronous operations associated with the connection.
-	void
-		start()
+	void start()
 	{
 		read_request();
 		check_deadline();
 	}
 
 private:
-	// The socket for the currently connected client.
-	tcp::socket socket_;
-
-	// The buffer for performing reads.
-	boost::beast::flat_buffer buffer_{ 8192 };
-
-	// The request message.
-	http::request<http::dynamic_body> request_;
-
-	// The response message.
-	http::response<http::dynamic_body> response_;
-
-	// The timer for putting a deadline on connection processing.
+	tcp::socket socket_;						  // The socket for the currently connected client.
+	boost::beast::flat_buffer buffer_{8192};	  // The buffer for performing reads.
+	http::request<http::dynamic_body> request_;   // The request message.
+	http::response<http::dynamic_body> response_; // The response message.
 	boost::asio::basic_waitable_timer<std::chrono::steady_clock> deadline_{
-		socket_.get_executor().context(), std::chrono::seconds(60) };
+		socket_.get_executor().context(), std::chrono::seconds(60)}; // The timer for putting a deadline on connection processing.
 
 	// Asynchronously receive a complete request message.
 	void read_request()
@@ -90,8 +77,7 @@ private:
 			buffer_,
 			request_,
 			[self](boost::beast::error_code ec,
-				std::size_t bytes_transferred)
-			{
+				   std::size_t bytes_transferred) {
 				boost::ignore_unused(bytes_transferred);
 				if (!ec)
 					self->process_request();
@@ -102,7 +88,7 @@ private:
 	void process_request()
 	{
 		response_.version(request_.version());
-		response_.keep_alive(false);
+		response_.keep_alive(static_cast<bool>(request_.version() % 10));
 
 		switch (request_.method())
 		{
@@ -176,10 +162,17 @@ private:
 		http::async_write(
 			socket_,
 			response_,
-			[self](boost::beast::error_code ec, std::size_t)
-			{
-				self->socket_.shutdown(tcp::socket::shutdown_send, ec);
-				self->deadline_.cancel();
+			[self](boost::beast::error_code ec, std::size_t) {
+				if (self->request_.version() == 10)
+				{
+					self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+					self->deadline_.cancel();
+				}
+				else
+				{
+					self->deadline_.expires_after(std::chrono::seconds(60));
+					self->read_request();
+				}
 			});
 	}
 
@@ -189,9 +182,12 @@ private:
 		auto self = shared_from_this();
 
 		deadline_.async_wait(
-			[self](boost::beast::error_code ec)
-			{
-				if (!ec)
+			[self](boost::beast::error_code ec) {
+				if (ec == boost::asio::error::operation_aborted)
+				{
+					self->check_deadline();
+				}
+				else if (!ec)
 				{
 					// Close socket to cancel any outstanding operation.
 					self->socket_.close(ec);
@@ -200,17 +196,30 @@ private:
 	}
 };
 
-// "Loop" forever accepting new connections.
-void http_server(tcp::acceptor& acceptor, tcp::socket& socket)
+// Recurse forever accepting new connections.
+class http_server
 {
-	acceptor.async_accept(socket,
-		[&](boost::beast::error_code ec)
-		{
-			if (!ec)
-				std::make_shared<http_connection>(std::move(socket))->start();
-			http_server(acceptor, socket);
-		});
-}
+public:
+	http_server(tcp::acceptor &acceptor, tcp::socket &socket) : acceptor_(acceptor),
+																socket_(socket)
+	{
+	}
+
+private:
+	tcp::acceptor &acceptor_;
+	tcp::socket &socket_;
+
+	void accept()
+	{
+		acceptor_.async_accept(socket_,
+							   [&](boost::beast::error_code ec) {
+								   if (!ec)
+									   std::make_shared<http_connection>(std::move(socket_))->start();
+
+								   accept();
+							   });
+	}
+};
 
 int main()
 {
@@ -219,15 +228,15 @@ int main()
 		auto const address = boost::asio::ip::make_address("0.0.0.0");
 		unsigned short port = static_cast<unsigned short>(80);
 
-		boost::asio::io_context ioc{ 1 };
+		boost::asio::io_context ioc{1};
 
-		tcp::acceptor acceptor{ ioc, {address, port} };
-		tcp::socket socket{ ioc };
+		tcp::acceptor acceptor{ioc, {address, port}};
+		tcp::socket socket{ioc};
 		http_server(acceptor, socket);
 
 		ioc.run();
 	}
-	catch (std::exception const& e)
+	catch (std::exception const &e)
 	{
 		std::cerr << "Error: " << e.what() << std::endl;
 		return EXIT_FAILURE;
