334c0ee0ed6d5d4da5857a6eea6dee984d35f755
Christopher McArthur <prince.chrismc@gmail.com>
2019-09-16 21:31:09 -0400

    trying to test for cache dir differently

--- ci/install-vcpkg.sh
+++ ci/install-vcpkg.sh
@@ -3,14 +3,12 @@ if [ ! -d "$1" ]; then
    echo "usage: $0 <PATH>"
 fi
 
-if [ ! -d "~/vcpkg" ]; then
-   cd ~/
+cd ~/vcpkg || (
+   cd ~/ &&
    git clone https://github.com/Microsoft/vcpkg.git &&
    cd vcpkg &&
    ./bootstrap-vcpkg.sh &&
    sudo ./vcpkg integrate install &&
    git apply -v --ignore-whitespace "$1"/vcpkg.patch
-else
-   cd ~/vcpkg &&
-   git pull;
-fi
+)
+git pull
