602cde2d5a51e28317434eeea8aed7afbc8b043b
Christopher McArthur <prince.chrismc@gmail.com>
2019-09-17 23:13:14 -0400

    removing extra headers

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,12 @@
 
 #include "server_certificate.hpp"
 
-#include <algorithm>
 #include <boost/asio/bind_executor.hpp>
-#include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/ssl/stream.hpp>
 #include <boost/asio/strand.hpp>
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
-#include <boost/beast/version.hpp>
-#include <boost/config.hpp>
-#include <cstdlib>
-#include <functional>
-#include <iostream>
+#include <boost/beast.hpp>
 #include <memory>
 #include <string>
-#include <vector>
 
 using tcp = boost::asio::ip::tcp;     // from <boost/asio/ip/tcp.hpp>
 namespace ssl = boost::asio::ssl;     // from <boost/asio/ssl.hpp>
