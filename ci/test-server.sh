a33a5da8ea6ef07623b1984b52bef4b05fe9c917
Christopher McArthur <prince.chrismc@gmail.com>
2019-09-20 08:52:33 -0400

    improving readability of test script

--- ci/test-server.sh
+++ ci/test-server.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
+# Shell script leverging curl and wget to verify basic HTTP/1.0 and HTTP/1.1
+# connection and keep alive headers
+
+alias curl-insecure="curl -k -i -s"
+alias wget-insecure="wget --no-check-certificate -q --save-headers -O -"
+
+BASE_HREF="https://localhost:8443"
+README_HREF="${BASE_HREF}/README.md"
+LICENSE_HREF="${BASE_HREF}/LICENSE"
 
 # Basic HTTP/1.0 response indicated connection closed
-[ $(curl -k --http1.0 -i -s https://localhost:8443/README.md https://localhost:8443/LICENSE | grep -o "Connection: closed" | wc -l) -eq 2 ] || (echo false && exit 1)
+[ $(curl-insecure --http1.0 "${BASE_HREF}" "${README_HREF}" "${LICENSE_HREF}" |
+   grep -o "Connection: closed" | wc -l) -eq 3 ] || (echo "Failed: HTTP/1.0" && exit 1)
 
 # Basic HTTP/1.1 response keep alive limit decrements
-[ $(curl -k --http1.1 -i -s https://localhost:8443/README.md https://localhost:8443/LICENSE | grep -o "Keep-Alive: timeout=60, max=49" | wc -l) -eq 1 ] || (echo false && exit 1)
-[ $(curl -k --http1.1 -i -s https://localhost:8443/README.md https://localhost:8443/LICENSE | grep -o "Keep-Alive: timeout=60, max=48" | wc -l) -eq 1 ] || (echo false && exit 1)
+HTTP_ONE_DOT_ONE_MULTIPLE_REQUESTS=$(curl-insecure --http1.1 "${README_HREF}" "${LICENSE_HREF}")
+[ $(echo "${HTTP_ONE_DOT_ONE_MULTIPLE_REQUESTS}" | grep -o "Keep-Alive: timeout=60, max=49" |
+   wc -l) -eq 1 ] || (echo "Failed: HTTP/1.1 - Limit" && exit 1)
+[ $(echo "${HTTP_ONE_DOT_ONE_MULTIPLE_REQUESTS}" | grep -o "Keep-Alive: timeout=60, max=48" |
+   wc -l) -eq 1 ] || (echo "Failed: HTTP/1.1 - Decrement" && exit 1)
 
 # Basic HTTP/1.1 response keep alive timeout reset connection
-[ $(wget --no-check-certificate -q -w 65 --save-headers -O - https://localhost:8443/README.md https://localhost:8443/LICENSE | grep -o "Keep-Alive: timeout=60, max=49" | wc -l) -eq 2 ] && echo true
+[ $(wget-insecure -w 65 "${README_HREF}" "${LICENSE_HREF}" | grep -o "Keep-Alive: timeout=60, max=49" |
+   wc -l) -eq 2 ] || (echo "Failed: HTTP/1.1 - Timeout" && exit 1)
