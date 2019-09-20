ec7a45432ff7cc453425cbab059b97bfb3773962
Christopher McArthur <prince.chrismc@gmail.com>
2019-09-20 09:02:40 -0400

    [skip ci] adding todo in test script

--- ci/test-server.sh
+++ ci/test-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Shell script leverging curl and wget to verify basic HTTP/1.0 and HTTP/1.1
-# connection and keep alive headers
+# behavoir such as connection and keep alive headers and HTTPS criterias
 
 alias curl-insecure="curl -k -i -s"
 alias wget-insecure="wget --no-check-certificate -q --save-headers -O -"
@@ -8,6 +8,12 @@ alias wget-insecure="wget --no-check-certificate -q --save-headers -O -"
 BASE_HREF="https://https.testserver.lan:8443"
 README_HREF="${BASE_HREF}/README.md"
 LICENSE_HREF="${BASE_HREF}/LICENSE"
+
+# HTTP does not answer
+
+# HTTPS reports correct hostname
+
+# HTTPS does not answer from incorrect name
 
 # Basic HTTP/1.0 response indicated connection closed
 [ $(curl-insecure --http1.0 "${BASE_HREF}" "${README_HREF}" "${LICENSE_HREF}" |
@@ -19,6 +25,8 @@ HTTP_ONE_DOT_ONE_MULTIPLE_REQUESTS=$(curl-insecure --http1.1 "${README_HREF}" "$
    wc -l) -eq 1 ] || (echo "Failed: HTTP/1.1 - Limit" && exit 1)
 [ $(echo "${HTTP_ONE_DOT_ONE_MULTIPLE_REQUESTS}" | grep -o "Keep-Alive: timeout=60, max=48" |
    wc -l) -eq 1 ] || (echo "Failed: HTTP/1.1 - Decrement" && exit 1)
+
+# Basic HTTP/1.1 response limit loops around
 
 # Basic HTTP/1.1 response keep alive timeout reset connection
 [ $(wget-insecure -w 65 "${README_HREF}" "${LICENSE_HREF}" | grep -o "Keep-Alive: timeout=60, max=49" |
