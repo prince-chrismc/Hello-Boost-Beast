9b6c021ed377a5117216b7bfa2b657680a9a67f1
Chris Mc <prince.chrismc@gmail.com>
2019-09-20 12:53:38 -0400

    adding git reset to test update of vcpkg

--- ci/update-or-install-vcpkg.sh
+++ ci/update-or-install-vcpkg.sh
@@ -3,7 +3,7 @@ if [ ! -d "$1" ]; then
    echo "usage: $0 <PATH>"
 fi
 
-(cd ~/vcpkg && git fetch && git pull) || (
+(cd ~/vcpkg git reset --hard 8a44d47f76e01b787ebb1fc71dfe36909fdd1793 && git fetch && git pull) || (
    cd ~/ &&
    git clone https://github.com/Microsoft/vcpkg.git &&
    cd vcpkg &&
