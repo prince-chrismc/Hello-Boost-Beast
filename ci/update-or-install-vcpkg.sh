76ee6c8471d339628fa762682bb5277bf53a50dc
Christopher McArthur <prince.chrismc@gmail.com>
2019-09-22 20:00:48 -0400

    improving detecting of vcpkg git repo

--- ci/update-or-install-vcpkg.sh
+++ ci/update-or-install-vcpkg.sh
@@ -3,19 +3,27 @@ if [ ! -d "$1" ]; then
    echo "usage: $0 <PATH>"
 fi
 
-$(cd ~/vcpkg && git fetch)
-VCPKG_REPO_EXISTS=$?
+VCPKG_REPO_EXISTS="FALSE"
 
-if [ $VCPKG_REPO_EXISTS ]; then
+if [ -d "$HOME/vcpkg/.git/" ]; then
+   VCPKG_REPO_EXISTS="TRUE"
+fi
+
+if [ $VCPKG_REPO_EXISTS = "TRUE" ]; then
    cd ~/vcpkg || exit 1
+   git fetch
    git pull --rebase
    ./vcpkg upgrade --no-dry-run
 else
    cd ~/ || exit 1
+   if [ -d "$HOME/vcpkg/" ]; then
+      rm -rf "$HOME/vcpkg/"
+   fi
+
    git clone https://github.com/Microsoft/vcpkg.git
    cd vcpkg || exit 1
    ./bootstrap-vcpkg.sh
-   sudo ./vcpkg integrate install
+   ./vcpkg integrate install
    git apply -v --ignore-whitespace "$1"/vcpkg.patch
    git commit -a -m "patching asio and beast version"
    export VCPKG_CXX_FLAGS="-std=c++11 -D _GLIBCXX_USE_CXX11_ABI=0"
