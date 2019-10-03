64d11dc9cc343a0eeb00f88dcecf35faa67ad1bc
Chris Mc <prince.chrismc@gmail.com>
2019-10-02 22:27:22 -0400

    [skip ci] adding white space

--- src/main.cpp
+++ src/main.cpp
@@ -65,11 +65,14 @@ boost::beast::string_view mime_type(boost::beast::string_view path)
 // Append an HTTP rel-path to a local filesystem path. The returned path is normalized for the platform.
 std::string path_cat(boost::beast::string_view base, boost::beast::string_view path)
 {
-  if (base.empty()) return path.to_string();
+  if (base.empty())
+	  return path.to_string();
+
   std::string result = base.to_string();
 #if BOOST_MSVC
   char constexpr path_separator = '\\';
-  if (result.back() == path_separator) result.resize(result.size() - 1);
+  if (result.back() == path_separator)
+	  result.resize(result.size() - 1);
   result.append(path.data(), path.size());
   for (auto& c : result)
     if (c == '/') c = path_separator;
