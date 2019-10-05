3f0fff14a8a0c670ece312b0eb9a73b318b22a85
Christopher McArthur <prince.chrismc@gmail.com>
2019-10-05 08:34:39 -0400

    [skip ci] trying to verify certificate
    using python

---
+++ ci/test-server.py
@@ -0,0 +1,25 @@
+import sys
+import requests
+
+if len(sys.argv) != 2:
+    print("Args: '{}' was not valid.".format(sys.argv), file=sys.stderr)
+    print("{0} <host>".format(sys.argv[0]))
+    exit(127)
+
+host = sys.argv[1]
+if not host.startswith("https://"):
+    host = "https://" + host
+
+print("Sending basic un-verified request...")
+r = requests.get(host + "/", verify=False)
+data = r.content  # Content of response
+print(r.status_code)  # Status code of response
+# print(data)
+
+print("Sending verified request...")
+rv = requests.get(
+    host + "/",
+    #  verify=False,
+    #  verify="ca/intermediate/certs/ca-chain.cert.pem")
+    verify="ca/intermediate/certs/")
+print(rv.status_code)  # Status code of response
