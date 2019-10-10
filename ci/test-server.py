08d9c06f4b9cb91cf4e328f8e77444a69973b454
Chris Mc <prince.chrismc@gmail.com>
2019-10-10 14:43:43 -0400

    correcting unsecured test logic

--- ci/test-server.py
+++ ci/test-server.py
@@ -14,13 +14,26 @@ if len(sys.argv) == 3:
     port = sys.argv[2]
 print("Targert: '{0}:{1}'".format(host, port))
 
-unsecure = "http://{0}:{1}".format(host, port)
-print("Sending unsecure HTTP to {}...".format(unsecure))
-r1 = requests.get(unsecure + "/", verify=False)
-if not r1.status_code == 404:
+# test_01
+try:
+    unsecure = "http://{0}:{1}/".format(host, port)
+    print("Sending unsecure HTTP to {}...".format(unsecure))
+    requests.get(unsecure)
+except requests.ConnectionError:
+    pass
+else:
     print("FAILED: Obtained a ({}) from server".format(r1.status_code))
     exit(1)
 
+# test_02
+unverified = "https://{0}:{1}".format(host, port)
+print("Sending HTTPs to {} without checking certificate...".format(unverified))
+r1 = requests.get(unverified, verify=False)
+if not r1.status_code == 404:
+    print("FAILED: Obtained a ({}) from server".format(r1.status_code))
+    exit(2)
+
+# test_03
 print("Trying to obtain certificate from {}:{}...".format(host, port))
 cert = ssl.get_server_certificate((host, port))
 x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
@@ -29,7 +42,7 @@ if b'CN' in subject:
     if not subject[b'CN'] == host:
         print("FAILED: certificate CN '{0}' did not match {1}."
               .format(x509.get_subject().get_components()['CN'], host))
-        exit(2)
+        exit(3)
 else:
     print("FAILED: certificate did not contain a common name (CN)")
-    exit(2)
+    exit(3)
