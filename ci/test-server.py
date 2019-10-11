020539e28ef0345405d38392e7271bc0196cd3c6
Christopher McArthur <prince.chrismc@gmail.com>
2019-10-10 23:16:38 -0400

    correcting common name verification in

--- ci/test-server.py
+++ ci/test-server.py
@@ -5,7 +5,8 @@ import OpenSSL
 
 if not 2 <= len(sys.argv) <= 3:
     print("Args: '{0}' was not valid.".format(sys.argv), file=sys.stderr)
-    print("Usage: {0} <host> <port>\n   host: Fully Qualifed Domain Name (FQDN)\n   port: optional. default 443\n".format(sys.argv[0]))
+    print("Usage: {0} <host> <port>\n   host: Fully Qualifed Domain Name (FQDN)\n   port: optional. default 443\n".format(
+        sys.argv[0]))
     exit(127)
 
 host = sys.argv[1]
@@ -38,11 +39,10 @@ print("Trying to obtain certificate from {}:{}...".format(host, port))
 cert = ssl.get_server_certificate((host, port))
 x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
 subject = x509.get_subject().get_components()
-if b'CN' in subject:
-    if not subject[b'CN'] == host:
-        print("FAILED: certificate CN '{0}' did not match {1}."
-              .format(x509.get_subject().get_components()['CN'], host))
-        exit(3)
-else:
-    print("FAILED: certificate did not contain a common name (CN)")
+if not (b'CN', host.encode()) in subject:
+    print("FAILED: certificate CN did not match {}.".format(host))
     exit(3)
+
+
+
+
