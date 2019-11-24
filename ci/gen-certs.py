b8a6e5c831080e91cc766f28fe72ac16370e4133
Christopher McArthur <prince.chrismc@gmail.com>
2019-11-23 21:40:48 -0500

    making basic directories

--- ci/gen-certs.py
+++ ci/gen-certs.py
@@ -1,8 +1,22 @@
+from shutil import which
+from os import makedirs
 import subprocess
 
-retval = subprocess.run(["which", "openssl"],
-                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-retval.check_returncode()
-openssl_path = retval.stdout.decode().strip()
 
-print(openssl_path)
+def find_openssl():
+    openssl_path = which("openssl")
+
+    # print(openssl_path)
+
+    retval = subprocess.run([openssl_path, "--version"])
+    if not retval.check_returncode() == 0:
+        exit()
+
+
+makedirs("ca/certs", exist_ok=True)
+makedirs("ca/newcerts", exist_ok=True)
+makedirs("ca/private", mode=0o700, exist_ok=True)
+
+open("ca/index.txt", "w+")
+with open("ca/serial", "w+") as f:
+    f.write("1000")
